65bfa92d90db21eef258a42c2253d1ac15a8552b
Илья Корытов <t.b.o.l@mail.ru>
2023-02-04 18:31:51 +0400

    управляющие символы

--- module-3/Program.cs
+++ module-3/Program.cs
@@ -5,6 +5,9 @@
     {
        const string MyName = "Илья";
         Console.WriteLine(MyName);
+        Console.WriteLine("\t Привет , мир!");
+        Console.WriteLine("\t Мне 28 лет");
+        Console.Write("\t Меня зовут \n Илья");
         Console.ReadKey();
     }
 }
