e271629e3d0b622a29a2fef817646e6b0794273b
Илья Корытов <t.b.o.l@mail.ru>
2023-02-04 18:01:31 +0400

    Добавьте файлы проекта.

---
+++ module-3/Program.cs
@@ -0,0 +1,11 @@
+﻿class MainClass
+{
+
+    static void Main(string[] args)
+    {
+        string MyName;
+        MyName = "Илья";
+        Console.WriteLine(MyName);
+        Console.ReadKey();
+    }
+}
