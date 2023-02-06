85cb68f5bd4936badd485145dbee098d84795d2c
Илья Корытов <t.b.o.l@mail.ru>
2023-02-06 17:59:24 +0400

    преобразование

---
+++ module3.7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите свое имя:");
+        string name = Console.ReadLine();
+
+        Console.Write("Введите свой возраст:");
+        byte age = checked((byte)int.Parse(Console.ReadLine()));
+
+        Console.WriteLine("Ваше имя {0} и вам {1} лет", name, age);
+
+        Console.WriteLine("Какой ваш любимый день недели?");
+            DayOfWeek day = (DayOfWeek) int.Parse(Console.ReadLine());
+        Console.WriteLine(" Ваш любимый день {0}", day);
+    }
+}
